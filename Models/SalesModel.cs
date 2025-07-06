79e159535402ac1df43949a1584c671138e41c57
unnamedKnight-47 <atikeprofessional117@gmail.com>
2025-07-06 12:41:20 +0600

    Add EntityGen model

--- Models/SalesModel.cs
+++ Models/SalesModel.cs
@@ -14,6 +14,5 @@ namespace Models
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal Discount { get; set; }
-
     }
 }
---
+++ ReportLib/ReportConfig00.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportLib
+{
+    public static class ReportConfig00
+    {
+        public static LocalReport GetLocalReport(string RptName, Object RptDataSet, Object RptDataSet2, Object UserDataset)
+        {
+            Assembly assembly1 = Assembly.LoadFrom("ReportLib.dll");
+
+            //accessing the rdlc file from the rptlib
+            Stream stream1 = assembly1.GetManifestResourceStream("ReportLib." + RptName + ".rdlc");
+            LocalReport Rpt1a = new LocalReport() { DisplayName = RptName };
+            Rpt1a.LoadReportDefinition(stream1);
+            Rpt1a.DataSources.Clear();
+
+
+            if (UserDataset != null)
+            {
+                var list3 = (List<string>)UserDataset;
+                Rpt1a.SetParameters(new ReportParameter("ParmHeader1", list3[0].RptEntName));
+                Rpt1a.SetParameters(new ReportParameter("ParmFooter1", list3[0].RptFooter1));
+            }
+
+            switch (Rpt1a.DisplayName.Trim())
+            {
+                //case "RptSetup.RptLocInfo1": Rpt1a = RptSetup_RptLocInfo1(Rpt1a, RptDataSet, RptDataSet2, UserDataset); break;
+                //case "RptSetup.RptAccInfo1": Rpt1a = RptSetup_RptAccInfo1(Rpt1a, RptDataSet, RptDataSet2, UserDataset); break;
+                case "RptSetup.RptResInfo1": Rpt1a = RptSetup_RptResInfo1(Rpt1a, RptDataSet, RptDataSet2, UserDataset); break;
+            }
+            return Rpt1a;
+
+
+        }
+
+    }
+}
