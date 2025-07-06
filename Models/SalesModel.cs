b3026c510c26358006a3090b472908aaf47ff2c9
unnamedKnight-47 <atikeprofessional117@gmail.com>
2025-07-06 11:46:06 +0600

    Add datamodel for the report

---
+++ Models/SalesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class SalesModel
+    {
+        public DateTime SaleDate { get; set; }
+        public string CustomerName { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+
+    }
+}
